c3f0124a8ad7b81bd1f72bd0067fe4541eaa3f72
joshebrani <joshuaebrani@gmail.com>
2021-02-15 11:02:20 -0500

    UsersController

--- reddit_clone/app/controllers/users_controller.rb
+++ reddit_clone/app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render :index
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render :show
+    end
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            sign_in!(@user)
+            redirect_to users_url
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
